ab8f9dc5553485c3e7ae2ce104826ce7cf7d0952
Davide <davide.ciulla@ticino.com>
2020-02-11 12:09:11 +0100

    major project update

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,34 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 
-function autoprefixerTask() {
-	return gulp
-		.src('./styles/style.css')
+function images() {
+	return gulp.src('./src/img/*.*')
+		.pipe(gulp.dest('./dist/img'))
+}
+
+function scripts() {
+	return gulp.src('./src/scripts/*.+(js|php)')
+		.pipe(gulp.dest('./dist/scripts'))
+}
+
+function html() {
+	return gulp.src('./src/*.html')
+		.pipe(gulp.dest('./dist'))
+}
+
+function css() {
+	return gulp.src('./src/styles/*.css')
 		.pipe(autoprefixer())
-		.pipe(gulp.dest('./app'))
+		.pipe(gulp.dest('./dist/styles'))
 }
 
 function watchTask() {
-	gulp.watch('./styles/*.css', autoprefixerTask);
+	gulp.watch('./src/styles/*.css', css);
 }
 
-exports.autoprefixerTask = autoprefixerTask;
+exports.images = images;
+exports.scripts = scripts;
+exports.html = html;
+exports.css = css;
 exports.watchTask = watchTask;
-exports.default = gulp.parallel(autoprefixerTask, watchTask);
+exports.default = gulp.parallel(images, scripts, html, gulp.parallel(css, watchTask));
